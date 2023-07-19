e7e7e2fa710d6f25d2cf4beca1fccdf92f20cc1d
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 14:39:25 +0200

    Fix t2a.rs macro_rules import and export
    Related commit message should be:
      * Fix t2a.rs macro_rules import and export

--- src/base/t2a.rs
+++ src/base/t2a.rs
@@ -2,20 +2,28 @@ use crate::using::*;
 
 macro_rules! _t2a_count_helper {
     () => (0usize);
-    ( $x:tt $($xs:tt)* ) => (1usize + _t2a_count_helper!($($xs)*));
+    ( $x:tt $($xs:tt)* ) => (1usize + $crate::base::t2a::_t2a_count_helper!($($xs)*));
 }
+pub(crate) use _t2a_count_helper;
 
 #[macro_export]
 macro_rules! t2a_count {
     ($($trait:ident),* $(,)?) => {
-        _t2a_count_helper!($($trait)*)
+        $crate::base::t2a::_t2a_count_helper!($($trait)*)
     };
 }
 
 #[macro_export]
 macro_rules! t2a_type {
     ($($trait:ident),* $(,)?) => {
-        [(::std::any::TypeId, [u8; ::std::mem::size_of::<*mut dyn ::std::error::Error>()]); t2a_count_helper!($($trait)*)]
+        [(::std::any::TypeId, [u8; ::std::mem::size_of::<*mut dyn ::std::error::Error>()]); $crate::t2a_count!($($trait)*)]
+    };
+}
+
+#[macro_export]
+macro_rules! t2a_type_vec {
+    () => {
+        ::std::vec::Vec<(::std::any::TypeId, [u8; ::std::mem::size_of::<*mut dyn ::std::error::Error>()])>
     };
 }
 
@@ -29,7 +37,21 @@ macro_rules! t2a_zero {
                     [0u8; ::std::mem::size_of::<*mut dyn $trait>()]
                 )
             ),*
-        ]
+        ] as $crate::t2a_type!($($trait)*)
+    };
+}
+
+#[macro_export]
+macro_rules! t2a_zero_vec {
+    ($($trait:ident),* $(,)?) => {
+        vec![
+            $(
+                (
+                    ::std::any::TypeId::of::<()>(),
+                    [0u8; ::std::mem::size_of::<*mut dyn $trait>()]
+                )
+            ),*
+        ] as $crate::t2a_type_vec!()
     };
 }
 
@@ -46,7 +68,24 @@ macro_rules! t2a_impl {
                     }
                 )
             ),*
-        ]
+        ] as $crate::t2a_type!($($trait)*)
+    };
+}
+
+#[macro_export]
+macro_rules! t2a_impl_vec {
+    ($struct:ident to [$($trait:ident),* $(,)?]) => {
+        [
+            $(
+                (
+                    ::std::any::TypeId::of::<dyn $trait>(),
+                    unsafe {
+                        let ptr = &mut $struct as &mut dyn $trait;
+                        ::std::mem::transmute_copy::<_, [u8; ::std::mem::size_of::<*mut dyn $trait>()]>(&ptr)
+                    }
+                )
+            ),*
+        ] as $crate::t2a_type_vec!()
     };
 }
 
