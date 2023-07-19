85b3af0edf41a05cb2c27661733433e497efb8dc
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 16:17:35 +0200

    Even more changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
 // Temporarly enabled
 #![allow(unused)]
 
-pub(crate) mod using;
 pub(crate) mod base;
+pub(crate) mod using;
 
 pub mod prelude;
-pub mod element;
 
 fn test() {
     use prelude::*;
+    use using::*;
+
+    let node = base::node::LueNode {
+        obj_self: OnceCell::new(),
+
+        upper: None,
+        upper_raw: null_mut(),
+
+        lower: vec![],
+        lower_raw: vec![],
+
+        layout: OnceCell::new(),
+        layout_raw: OnceCell::new(),
+        layout_t2a: OnceCell::new(),
+
+        system: todo!(),
+    };
 }
