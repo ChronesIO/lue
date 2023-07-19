f389df5d3fbe9ba8a6b6fa3ab6f73896fc7c7a7b
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 14:38:00 +0200

    LueElement enhancements to handle element attachment/detachment
    The LueElement trait in element module has been updated to include additional methods. This modification enables LueElement to handle self-attachment/detachment and attachment/detachment of a node. This improvement allows for better management of element states when attached or detached from a node.
    
    Changes:
    - Added `host_ref` method to return a reference to the hosting LueNode
    - Added `host_mut` method to return a mutable reference to the hosting LueNode
    - Added `_on_attached_self` method to handle self-attachment logic
    - Added `_on_detached_self` method to handle self-detachment logic
    - Added `_on_attached_node` method to handle attachment of a specific node
    - Added `_on_detached_node` method to handle detachment of a specific node
    
    These enhancements improve the functionality of LueElement and provide more control over element attachment/detachment.

--- src/base/element.rs
+++ src/base/element.rs
@@ -1,3 +1,12 @@
+use crate::{base::node::LueNode, using::*};
+
 pub trait LueElement {
-    
+    fn host_ref(&self) -> &LueNode;
+    fn host_mut(&mut self) -> &mut LueNode;
+
+    fn _on_attached_self(&mut self);
+    fn _on_detached_self(&mut self);
+
+    fn _on_attached_node(&mut self, node: &Arc<LueNode>);
+    fn _on_detached_node(&mut self, node: &Arc<LueNode>);
 }
