abd3bb04f74e94212805c4c61d140e4148b48a71
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 14:37:36 +0200

    Refactored LueNode structure, added new fields and methods
    The LueNode structure in `node.rs` has been refactored to include new fields and methods.
    
    - The `obj_self` field has been converted to an object type using the `obj_type!` macro.
    - Two new public fields, `upper` and `upper_raw`, have been added to store an optional arc reference to the upper node and a raw pointer to the upper node, respectively.
    - Two new public fields, `lower` and `lower_raw`, have been added to store a vector of arc references to lower nodes and a vector of raw pointers to lower nodes, respectively.
    - Two new fields, `layout` and `layout_raw`, have been added to store an `OnceCell` arc reference and a raw pointer, respectively, to an object implementing the `LueLayout` trait.
    - A new field, `layout_t2a`, has been added to store an `OnceCell` type-to-arc vector.
    - The `system` field's type has been changed to `Arc<dyn Any>`.
    - The `LueNode` structure has been implemented with arc reference using the `obj_impl!` macro.
    - Added two public methods, `has_lower` and `has_lower_ref`, to check if a node has a lower node or a specific lower node reference, respectively.
    - Added two internal methods, `_attach_node` and `_detach_node`, to attach and detach a lower node, respectively.
    - Added two internal methods, `_attach_self` and `_detach_self`, to attach and detach the upper node, respectively.
    - Added four internal methods, `_on_attached_node`, `_on_detached_node`, `_on_attached_self`, and `_on_detached_self`, to trigger events in the layout when lower or upper nodes are attached or detached, respectively.
    
    These changes improve the flexibility and functionality of the LueNode structure, allowing for easier manipulation of node relationships and providing hooks for layout event triggers.
    
    Refactor LueNode structure, add fields and methods

--- src/base/node.rs
+++ src/base/node.rs
@@ -1,19 +1,116 @@
-use crate::using::*;
+use std::{error::Error, ops::Deref};
+
+use crate::{base::layout::LueLayout, obj_impl, obj_type, t2a_type_vec, using::*};
 
 pub struct LueNode {
-    obj_self: OnceCell<ArcWeak<Self>>,
+    obj_self: obj_type!(Self with ArcWeak),
 
-    upper: Option<Arc<Self>>,
-    upper_raw: *mut Self,
+    pub upper: Option<Arc<Self>>,
+    pub upper_raw: *mut Self,
 
-    lower: Vec<Arc<Self>>,
-    lower_raw: Vec<*mut Self>,
+    pub lower: Vec<Arc<Self>>,
+    pub lower_raw: Vec<*mut Self>,
+
+    pub layout: OnceCell<Arc<dyn LueLayout>>,
+    pub layout_raw: OnceCell<*mut dyn LueLayout>,
+    pub layout_t2a: OnceCell<t2a_type_vec!()>,
 
     system: Arc<dyn Any>, // Todo: Change type to system
 }
+obj_impl!(LueNode with Arc);
 
 impl LueNode {
-    fn a() {
-        let t = TypeId::of::<String>();
+    pub fn has_lower(&self, node: &Arc<Self>) -> bool {
+        self.has_lower_ref(node.as_ref())
+    }
+    pub fn has_lower_ref(&self, node_ref: &Self) -> bool {
+        let node_ptr = node_ref as *const _;
+        match self.lower_raw.iter().find(|&&x| x as *const _ == node_ptr) {
+            Some(_) => true,
+            None => false,
+        }
+    }
+    
+    pub(crate) fn _attach_node(
+        &mut self,
+        node: &Arc<Self>,
+        index: Option<usize>,
+    ) -> Result<(), ()> {
+        if self.has_lower(node) {
+            return Err(());
+        }
+
+        if let Some(s) = index {
+            self.lower.insert(s, node.clone());
+            self.lower_raw
+                .insert(s, unsafe { transmute(node.as_ref() as *const _) });
+        } else {
+            self.lower.push(node.clone());
+            self.lower_raw
+                .push(unsafe { transmute(node.as_ref() as *const _) });
+        }
+
+        Ok(())
+    }
+    pub(crate) fn _detach_node(&mut self, node: &Arc<Self>) -> Result<(), ()> {
+        let node_ref = node.as_ref() as *const _;
+        if let Some(s) = self
+            .lower_raw
+            .iter()
+            .position(|&x| x as *const _ == node_ref)
+        {
+            self.lower.remove(s);
+            self.lower_raw.remove(s);
+            return Ok(());
+        }
+        Err(())
+    }
+
+    pub(crate) fn _attach_self(&mut self, node: &Arc<Self>) -> Result<(), ()> {
+        if self.upper.is_some() {
+            return Err(());
+        }
+
+        let node_ref = node.as_ref() as *const _;
+        self.upper = Some(node.clone());
+        self.upper_raw = unsafe { transmute(node_ref) };
+
+        Ok(())
+    }
+    pub(crate) fn _detach_self(&mut self) -> Result<(), ()> {
+        if self.upper.is_none() {
+            return Err(());
+        }
+
+        self.upper = None;
+        self.upper_raw = null_mut();
+
+        Ok(())
+    }
+
+    pub(crate) fn _on_attached_node(&mut self, node: &Arc<Self>) {
+        // trigger event in layout
+        unsafe {
+            self.layout.get().unwrap_unchecked()._on_attached_node(node);
+        }
+    }
+    pub(crate) fn _on_detached_node(&mut self, node: &Arc<Self>) {
+        // trigger event in layout
+        unsafe {
+            self.layout.get().unwrap_unchecked()._on_detached_node(node);
+        }
+    }
+
+    pub(crate) fn _on_attached_self(&mut self, node: &Arc<Self>) {
+        // trigger event in layout
+        unsafe {
+            self.layout.get().unwrap_unchecked()._on_attached_self(node);
+        }
+    }
+    pub(crate) fn _on_detached_self(&mut self, node: &Arc<Self>) {
+        // trigger event in layout
+        unsafe {
+            self.layout.get().unwrap_unchecked()._on_detached_self(node);
+        }
     }
 }
