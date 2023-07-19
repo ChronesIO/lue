ea82912e3fba022f33ecc1acf139d459af83eaeb
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 14:39:09 +0200

    Refactor the LueSystem struct and its implementation
    The LueSystem struct and its implementation have been refactored. The changes include:
    
    - The usage of `import` statements has been updated.
    - The LueSystem struct now includes a field `obj_self` of type `obj_type!(Self with ArcWeak)`.
    - The LueSystem struct now implements `obj_impl!(LueSystem with Arc)`.
    - The `attach_node()` function has been modified to take references to `Arc<LueNode>`, `Arc<LueNode>`, and `Option<usize>`.
    - Error handling has been added to the `attach_node()` and `detach_node()` functions.
    - The `detach_node()` function now gets the upper node first and performs various detachment operations.
    - The `add_context()` and `remove_context()` functions have been implemented, but they currently do not have any functionality.
    
    Ignore any changes to imports and requires.

--- src/base/system.rs
+++ src/base/system.rs
@@ -1,10 +1,62 @@
-use crate::using::*;
+use crate::{base::node::LueNode, obj_impl, obj_type, using::*};
 
-pub trait LueSystem {
-    fn attach_node(node: dyn Any, upper: dyn Any);
-    fn detach_node(node: dyn Any);
-    fn can_attach_node(node: dyn Any, upper: dyn Any);
+pub struct LueSystem {
+    obj_self: obj_type!(Self with ArcWeak),
+}
+obj_impl!(LueSystem with Arc);
 
-    fn add_context(context: dyn Any);
-    fn remove_context(context: dyn Any);
+impl LueSystem {
+    fn attach_node(
+        node: &Arc<LueNode>,
+        upper: &Arc<LueNode>,
+        index: Option<usize>,
+    ) -> Result<(), ()> {
+        // attach upper to lower first
+        if upper._attach_node(node, index).is_err() {
+            return Err(());
+        }
+
+        // attach lower to upper
+        if node._attach_self(upper).is_err() {
+            // revert changes
+            upper._detach_node(node);
+            return Err(());
+        }
+
+        // fire events
+        upper._on_attached_node(node);
+        node._on_attached_self(upper);
+
+        Ok(())
+    }
+    fn detach_node(node: &Arc<LueNode>) -> Result<(), ()> {
+        // get upper node first
+        let upper = node.as_ref().upper;
+
+        // check if lower contains upper
+        if upper.is_none() {
+            return Err(());
+        }
+        
+        let mut upper = upper.unwrap();
+
+        // detach at upper
+        if upper._detach_node(node).is_err() {
+            return Err(());
+        }
+
+        // detach at lower
+        if node._detach_self().is_err() {
+            // this should never happen
+            panic!("Could not detach node from parent: `node._detach_self` failed");
+        }
+
+        upper._on_detached_node(node);
+        node._on_detached_self(&upper);
+
+        Ok(())
+    }
+
+    fn add_context(context: Box<dyn Any>) {}
+    fn remove_context(context: Box<dyn Any>) {}
 }
