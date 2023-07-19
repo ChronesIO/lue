fefe46e30d4a820f73dd6def2b93f59079f8d2a4
CXCubeHD <cxcubehd@gmail.com>
2023-07-19 14:38:29 +0200

    Refactor LueStandardLayout implementation of LueLayout
    This commit refactors the implementation of the `LueLayout` trait for the `LueStandardLayout` struct in `layout.rs` file. The changes introduced include the addition of the `host_ref()` and `host_mut()` methods, as well as the `_on_attached_node()`, `_on_detached_node()`, `_on_attached_self()`, and `_on_detached_self()` functions. These changes aim to improve the overall structure and functionality of the `LueLayout` trait implementation.

--- src/base/layout.rs
+++ src/base/layout.rs
@@ -1,3 +1,56 @@
+use crate::{base::node::LueNode, obj_impl, obj_type, using::*};
+
 pub trait LueLayout {
-    
+    fn host_ref(&self) -> &LueNode;
+    fn host_mut(&mut self) -> &mut LueNode;
+
+    fn _on_attached_node(&mut self, node: &Arc<LueNode>);
+    fn _on_detached_node(&mut self, node: &Arc<LueNode>);
+
+    fn _on_attached_self(&mut self, node: &Arc<LueNode>);
+    fn _on_detached_self(&mut self, node: &Arc<LueNode>);
+}
+
+pub struct LueStandardLayout {
+    obj_self: obj_type!(Self with ArcWeak),
+
+    host: OnceCell<*mut LueNode>,
+
+    taffy: Arc<Taffy>,
+}
+obj_impl!(LueStandardLayout with Arc);
+
+impl LueLayout for LueStandardLayout {
+    fn host_ref(&self) -> &LueNode {
+        unsafe {
+            self.host
+                .get()
+                .unwrap_unchecked()
+                .as_ref()
+                .unwrap_unchecked()
+        }
+    }
+    fn host_mut(&mut self) -> &mut LueNode {
+        unsafe {
+            self.host
+                .get()
+                .unwrap_unchecked()
+                .as_mut()
+                .unwrap_unchecked()
+        }
+    }
+
+    fn _on_attached_node(&mut self, node: &Arc<LueNode>) {
+        todo!()
+    }
+    fn _on_detached_node(&mut self, node: &Arc<LueNode>) {
+        todo!()
+    }
+
+    fn _on_attached_self(&mut self, node: &Arc<LueNode>) {
+        todo!()
+    }
+    fn _on_detached_self(&mut self, node: &Arc<LueNode>) {
+        todo!()
+    }
 }
